5cc0fd68dcd90d256a559b266c23e711af2d431d
Pravleen <47741189+Pravleen@users.noreply.github.com>
2020-07-09 16:08:19 +0530

    Add files via upload

---
+++ basics/Identifier.java
@@ -0,0 +1,12 @@
+/*All predefined java class names and interface names, we can use as java identifiers. 
+Even though it is valid but it is not a good programming practice. 
+//It reduces readability and creates confusion*/
+class Identifier
+{ 
+public static void main(String args[]) 
+{  int String =100;  
+int Runnable=200; 
+System.out.println(String);
+ System.out.println(Runnable);
+ } 
+ } 
