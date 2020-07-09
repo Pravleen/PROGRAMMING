c8fcdb910d1feea4dd0d7fd1fe775d618e45b202
Pravleen <47741189+Pravleen@users.noreply.github.com>
2020-07-09 16:04:12 +0530

    Add files via upload

---
+++ basics/ReserveW.java
@@ -0,0 +1,13 @@
+//We cannot use reserve words like for if class as an identifier in java
+class ReserveW 
+{ 
+public static void main(String args[]) 
+{ 
+int for =10; 
+int if =20; 
+int class =50; 
+int cat =70;
+}
+} 
+ 
+ 
