3215b0b3b9e4a205a8e3805b4f3cfee6c6a60bcf
Pravleen <47741189+Pravleen@users.noreply.github.com>
2020-08-14 23:26:36 +0530

    Add files via upload

---
+++ Constructors/Constructor1.java
@@ -0,0 +1,19 @@
+//Creating Constructor Faculty()
+class Faculty
+{
+	Faculty()//same name as the class but without return type
+	{
+	String[] str1= new String[] {"add","vbhd"};
+	System.out.println(str1[0]);
+	Student.main(str1);
+	}
+}
+	
+ class Student
+{
+	public static void main(String[] args)
+	{
+		System.out.println("hi");
+		
+	}
+}
